4dc3c7387d0ec773644b361944eaa299c66b31fd
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-09 18:13:41 +0530

    Working Navigation Bar

---
+++ JS-files/script.js
@@ -0,0 +1,13 @@
+const menuBtn = document.getElementById('burger-container');
+const navOverlay =document.getElementById('nav');
+const navList = document.getElementsByClassName('menu')[0];
+const burgerBar = document.getElementsByClassName('hamRotate')[0];
+console.log(menuBtn,navOverlay,navList);
+
+function overlay() {
+    navOverlay.classList.toggle('invisible');
+    burgerBar.classList.toggle('active');
+}
+
+menuBtn.addEventListener('click',overlay);
+navList.addEventListener('click',overlay);
