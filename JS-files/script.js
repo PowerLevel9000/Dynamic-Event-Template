48430b59737fa7ca6e4220fab53c433814fb8aea
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-10 16:40:57 +0530

    overall body, add dynamics

--- JS-files/script.js
+++ JS-files/script.js
@@ -1,14 +1,40 @@
-const menuBtn = document.getElementById('burger-container');
-const navOverlay =document.getElementById('nav');
-const navList = document.getElementsByClassName('menu')[0];
-const burgerBar = document.getElementsByClassName('hamRotate')[0];
-console.log(menuBtn,navOverlay,navList);
 
-function overlay() {
-    navOverlay.classList.toggle('invisible');
-    burgerBar.classList.toggle('active');
-    navList.style.animation = 'awesome-out 2s'
+const trainer = document.querySelectorAll('.trainer-card');
+const button = document.querySelector('.trainer-btn');
+const less = document.getElementById('morles');
+const up = document.getElementById('chevron')
+console.log(trainer)
+function display() {
+    for (let i = 2; i < trainer.length; i++) {
+        trainer[i].classList.toggle('invisible')
+    }
+    if (less.innerText === 'MORE ' && up.classList.value === "fas fa-chevron-down") {
+        less.innerText = 'LESS ';
+        up.classList.value = "fas fa-chevron-up"
+    }else if (less.innerText === 'LESS ' && up.classList.value === "fas fa-chevron-up") {
+        less.innerText = 'MORE ';
+        up.classList.value = "fas fa-chevron-down"
+    }
 }
 
-menuBtn.addEventListener('click',overlay);
-navList.addEventListener('click',overlay);
+button.addEventListener('click', display)
+
+if (window.innerWidth <= 768) {
+    const menuBtn = document.getElementById('burger-container');
+    const navOverlay = document.getElementById('nav');
+    const navList = document.getElementsByClassName('menu')[0];
+    const burgerBar = document.getElementsByClassName('hamRotate')[0];
+    console.log(menuBtn, navOverlay, navList);
+    function overlay() {
+
+        navOverlay.classList.toggle('invisible');
+        burgerBar.classList.toggle('active');
+        navOverlay.style.animation = 'awesome-out 1s'
+
+    }
+    menuBtn.addEventListener('click', overlay);
+    navList.addEventListener('click', overlay);
+    console.log(hello);
+
+}
+
