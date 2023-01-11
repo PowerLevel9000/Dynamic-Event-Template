5fc9f919702004587404181ecc85d33d95c9959a
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-11 14:02:23 +0530

    dynamics and animation updated

--- JS-files/about.js
+++ JS-files/about.js
@@ -1,17 +1,14 @@
-if (window.innerWidth <= 768) {
-    const menuBtn = document.getElementById('burger-container');
-    const navOverlay = document.getElementById('nav');
-    const navList = document.getElementsByClassName('menu')[0];
-    const burgerBar = document.getElementsByClassName('hamRotate')[0];
-    function overlay() {
-
-        navOverlay.classList.toggle('invisible');
-        burgerBar.classList.toggle('active');
-        navOverlay.style.animation = 'awesome-out 1s'
-
-    }
-    menuBtn.addEventListener('click', overlay);
-    navList.addEventListener('click', overlay);
-
+const menuBtn = document.getElementById('burger-container');
+const navOverlay = document.getElementById('nav');
+const navList = document.getElementsByClassName('menu')[0];
+const burgerBar = document.getElementsByClassName('hamRotate')[0];
+function overlay() {
+  navOverlay.classList.toggle('invisible');
+  burgerBar.classList.toggle('active');
+  navOverlay.style.animation = 'awesome-out 1s';
 }
 
+if (window.innerWidth <= 768) {
+  menuBtn.addEventListener('click', overlay);
+  navList.addEventListener('click', overlay);
+}
