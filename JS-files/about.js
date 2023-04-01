680978f93d7700a62ee715253aa38351c54720bc
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-01 15:21:34 +0530

    Linter Errors and About Dynamics

--- JS-files/about.js
+++ JS-files/about.js
@@ -1,8 +1,8 @@
 const aboutDatabase = {
-  introduction:{
-    preTitle:'Namaste India!',
-    title:'SAI Organizing The<br>Summer-Camp 2023',
-    description:`
+  introduction: {
+    preTitle: 'Namaste India!',
+    title: 'SAI Organizing The<br>Summer-Camp 2023',
+    description: `
       This is website build for the SIA Summer Camp event at Jawaharlal Nehru Stadium (Delhi).
       Jawaharlal Nehru Stadium is a multi-sports stadium located in New Delhi, India.
       It is named after the first Prime Minister of India. Primarily a venue for
@@ -11,31 +11,25 @@ const aboutDatabase = {
       So what are you Waiting for hit registration button or grab your sit with tickets.
       gear up! gear up! gear up...!
     `,
-    manager:'Adarsh',
-    managerEmail:'adi8090808766@gmail.com'
-  }
-}
+    manager: 'Adarsh',
+    managerEmail: 'adi8090808766@gmail.com',
+  },
+};
 
-const aboutIntroduction =document.getElementById('aboutIntroduction');
+const aboutIntroduction = document.getElementById('aboutIntroduction');
 aboutIntroduction.innerHTML += `
   <div class="namaste" data-aos="fade-up">
-      <h2 title="Greeting">Namaste India!</h2>
-      <h1 title="Our Program">SAI Organizing The<br>Summer-Camp 2023</h1>
+      <h2 title="Greeting">${aboutDatabase.introduction.preTitle}</h2>
+      <h1 title="Our Program">${aboutDatabase.introduction.title}</h1>
     </div>
 
     <div class="txt-container" data-aos="fade-up">
       <p title="Website Description">
-        This is website build for the SIA Summer Camp event at Jawaharlal Nehru Stadium (Delhi).
-        Jawaharlal Nehru Stadium is a multi-sports stadium located in New Delhi, India.
-        It is named after the first Prime Minister of India. Primarily a venue for
-        football and athletics. We are organizing many sports and entertainment events
-        in summer-Camp. tickets and regeneration form are going to be vanish soon.
-        So what are you Waiting for hit registration button or grab your sit with tickets.
-        gear up! gear up! gear up...!
+        ${aboutDatabase.introduction.description}
       </p>
     </div>
     <p title="Doubt ??" class="contact" data-aos="fade-up">
       If you have further questions about the re-opening event please reach
-      out to our volunteer<br><a title="Mail our Manager" href="mailto:adi8090808766@gmail.com">Adarsh</a>
+      out to our volunteer<br><a title="Mail our Manager" href="mailto:${aboutDatabase.introduction.managerEmail}">${aboutDatabase.introduction.manager}</a>
     </p>
-`
+`;
