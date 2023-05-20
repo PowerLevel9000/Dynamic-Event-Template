3aa83385e09e0e6ab906621c8c4d942bc724eacb
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-05-20 09:26:57 +0530

    Linter fix

--- JS-files/translator.js
+++ JS-files/translator.js
@@ -1,5 +1,5 @@
-//  Google translator 
-
+//  Google translator all variable are in external source
+/* eslint-disable */
 function googleTranslateElementInit() {
   new google.translate.TranslateElement(
     { pageLanguage: 'en' },
